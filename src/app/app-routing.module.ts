359aac72cb20e82a8267245c2f1fa227100d2167
harshil <harshilshah186@gmail.com>
2019-02-08 11:38:32 +0530

    Login Module created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TtsCallComponent } from './tts-call/tts-call.component';
 import { HomeComponent } from './home/home.component';
+import {LoginComponent} from './login/login.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 const routes: Routes = [
+  {path: 'login', component: LoginComponent},
   {path: 'home', component: HomeComponent},
   { path: 'text-to-speech-call', component: TtsCallComponent },
   { path: '', redirectTo: '/home',pathMatch: 'full'},
